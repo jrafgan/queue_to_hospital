2557a20f43bdba1245d6b61d1bef6469d0e5499b
Maksat Duishenaliev <maksatovy@gmail.com>
2019-02-12 08:32:20 +0600

    написан код для распарсинга массива DOCTORS

--- src/components/Body/MiddleBar/MiddleBar.js
+++ src/components/Body/MiddleBar/MiddleBar.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
-const MiddleBar = () => {
+const MiddleBar = (props) => {
+    if (props.flag !== null)console.log(Object.values(props.docs));
     return (
-        <div className='middle_bar'>Middle bar</div>
+        <div className='middle_bar'>
+            <p>Врачи :</p>
+            {props.flag !== null ? <ul>
+                {Object.values(props.docs).map((item, index)=>{
+
+
+
+                   return <li key={index}><ul className='ul'>{Array.isArray(item) ? item.map((item2, index)=>{if (index === 0) {
+                       return <li className='show' onClick={props.onClick}>{item2}</li>
+                   } else {
+                       return <li className='hide'>{item2}</li>
+                   }
+                   }) : null}</ul></li>
+                })
+                }
+                </ul> : null}
+        </div>
     );
 };
 
